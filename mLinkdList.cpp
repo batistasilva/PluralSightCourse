fa09188c46bfda076941348b52a61a67a3863836
batistasilva <batista.freebsd@gmail.com>
2022-09-02 10:10:40 -0300

    First files for repo

---
+++ mLinkdList.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   mLinkdList.cpp
+ * Author: batista
+ *
+ * Created on 2 de setembro de 2022, 09:49
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
